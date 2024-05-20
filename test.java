d26b75c051a2b8e20afffa2c90c37885753d7398
karu1325 <143956677+karu1325@users.noreply.github.com>
2024-05-20 16:54:55 +0000

    [Add]for.java

--- test.java
+++ test.java
@@ -1,5 +1,16 @@
 public class test {
-  public static void main(String []args) {
-    System.out.println("Hello, World!");
+
+  public static void main(String[] args) {
+    String[] names = {"Bob", "John", "Lily"};
+    for (String name:names) {
+      System.out.println("Hello" + name);
+    }
+    num();
+  }
+
+  public static void num() {
+    for (int i = 1; i <= 5; i++) {
+      System.out.println(i);
+    }
   }
 }
